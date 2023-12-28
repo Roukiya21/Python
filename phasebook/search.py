6fe0095f164d5fac57820091ab883918be602c6d
Roukiya21 <152247664+Roukiya21@users.noreply.github.com>
2023-12-28 21:34:56 +0800

    Update search.py

--- phasebook/search.py
+++ phasebook/search.py
@@ -27,4 +27,45 @@ def search_users(args):
 
     # Implement search here!
 
-    return USERS
+    results = []
+    included_ids = set()
+
+    for user in USERS:
+        user_id = str(user["id"])
+
+
+        if "id" in args and args["id"] == user_id:
+            results.append((user, "id"))
+            included_ids.add(user_id)
+            continue
+
+
+        if "name" in args and args["name"].lower() in user["name"].lower():
+            if user_id not in included_ids:
+                results.append((user, "name"))
+                included_ids.add(user_id)
+            continue
+
+
+        if "age" in args:
+            try:
+                age = int(args["age"])
+                if (user["age"] - 1) <= age <= (user["age"] + 1) and user_id not in included_ids:
+                    results.append((user, "age"))
+                    included_ids.add(user_id)
+            except ValueError:
+                pass  
+
+
+        if "occupation" in args and args["occupation"].lower() in user["occupation"].lower():
+            if user_id not in included_ids:
+                results.append((user, "occupation"))
+                included_ids.add(user_id)
+
+
+    sorted_results = sorted(results, key=lambda x: ("id", "name", "age", "occupation").index(x[1]))
+
+
+    sorted_users = [{"id": str(user["id"]), "name": user["name"], "age": user["age"], "occupation": user["occupation"]} for user, _ in sorted_results]
+
+    return sorted_users

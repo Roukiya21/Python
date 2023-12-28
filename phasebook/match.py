14195dd6cfbb5b2a92b154e2ad80b8769b67c8b6
Roukiya21 <152247664+Roukiya21@users.noreply.github.com>
2023-12-28 21:33:38 +0800

    Update match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -7,7 +7,7 @@ from .data.match_data import MATCHES
 bp = Blueprint("match", __name__, url_prefix="/match")
 
 
-@bp.route("<int:match_id>")
+@bp.route("/<int:match_id>")
 def match(match_id):
     if match_id < 0 or match_id >= len(MATCHES):
         return "Invalid match id", 404
@@ -20,8 +20,7 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    common_numbers = set(fave_numbers_1) & set(fave_numbers_2)
+    
 
-    return True
+    return set(fave_numbers_2) <= common_numbers
